53ba84019eb3cf6065cddf9112ed1ab25336efd3
duongnguyen1216@gmail.com <duongnguyen1216@gmail.com>
2021-07-28 09:12:20 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+
+const express = require('express')
+const app = express()
+const port = 8080
+
+app.use(express.static('public'))
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// app.get('/recharge', (req, res) => {
+//   res.send('callback fid');
+// })
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
